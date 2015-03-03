5176d939ff4f531fd9cc80dc420f3f7aa7a33ece
Will <lishuzu@gmail.com>
2015-03-03 20:26:26 +1300

    diamond

--- computer&science/COMP589/Week1/diamond.sh
+++ computer&science/COMP589/Week1/diamond.sh
@@ -1,7 +1,38 @@
-#!/bin/bash
-for (( i = 0; i < 6; i++ )); do
-	padLength=$((6 + $i))
-	str=$(printf '%0.s .' $(seq 1 ${i-1}))
-	# echo $str
-	printf "%${padLength}s\n" $str
+#!/bin/sh
+i=1
+line=7
+mark='.'
+# up-triangle
+# line
+while [ $i -le $line ] ; do
+	# the number of points plus the number of spaces equal to line number -1 because the first line have no points
+	j=1
+	while [ $j -le $(($line-$i)) ] ; do
+		# -n    Do not print the trailing newline character. 
+		echo -n ' '
+		j=$(($j+1))
+	done
+	j=1
+	while [ $j -le $(($i-1)) ] ; do
+		echo -n " "$mark
+		j=$(($j+1))
+	done
+	printf "\n"
+	i=$(($i+1))
 done
+
+# down-triangle
+# use for to replace while loop
+
+for (( i = $line; i > 0; i-- )); do
+	for (( j = 0; j < $(($line-$i)); j++ )); do
+		echo -n ' '
+	done
+	for (( j = 0; j < $(($i-1)); j++ )); do
+		echo -n " "$mark
+	done
+	echo 
+done
+
+
+exit 0
