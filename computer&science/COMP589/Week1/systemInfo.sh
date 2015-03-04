9e93828a18b2f814620b218ea192cc621cb7d224
Will <lishuzu@gmail.com>
2015-03-04 19:16:24 +1300

    sys

--- computer&science/COMP589/Week1/systemInfo.sh
+++ computer&science/COMP589/Week1/systemInfo.sh
@@ -3,12 +3,18 @@ finger
 echo "Current shell:" $SHELL
 
 echo "Home directory:" $HOME
-echo "Operating system type:" $HOME
-echo "Current path setting:" $HOME
-echo "Current working directory:" $HOME
-echo "Currently logged number of users:" $HOME
-echo "Os info:" $HOME
-echo "All available shells:" $HOME
+echo "Operating system type:"
+uname -mrs
+echo "Current path setting:"
+echo $PATH
+echo "Current working directory:"
+pwd
+echo "Currently logged number of users:"
+users | wc -l
+echo "Os info:"
+uname -a
+echo "All available shells:"
+compgen -c
 echo "Mouse settings:" $HOME
 echo "CPU infomation:" $HOME
 echo "Memory information:" $HOME
