e05ca7e755fd10b8d594c9ae17563b6745984b9d
Will <lishuzu@gmail.com>
2015-03-04 20:10:17 +1300

    finish everthing but harddisk info because of permission

--- computer&science/COMP589/Week1/systemInfo.sh
+++ computer&science/COMP589/Week1/systemInfo.sh
@@ -1,22 +1,58 @@
+function splitLine {
+        printf "%0.1s" "-"{1..100}
+        echo
+}
+
 echo "Current Users"
 finger
+
+splitLine
+
 echo "Current shell:" $SHELL
+splitLine
 
 echo "Home directory:" $HOME
+splitLine
 echo "Operating system type:"
 uname -mrs
+splitLine
 echo "Current path setting:"
 echo $PATH
+splitLine
+
 echo "Current working directory:"
 pwd
+splitLine
+
 echo "Currently logged number of users:"
 users | wc -l
+splitLine
+
 echo "Os info:"
 uname -a
+splitLine
+
 echo "All available shells:"
 compgen -c
-echo "Mouse settings:" $HOME
-echo "CPU infomation:" $HOME
-echo "Memory information:" $HOME
-echo "hard disk information:" $HOME
-echo "File system:" $HOME
+splitLine
+
+echo "Mouse settings:"
+xinput list-props "Logitech USB Optical Mouse"
+splitLine
+
+echo "CPU infomation:"
+cat /proc/cpuinfo
+splitLine
+
+echo "Memory information:"
+cat /proc/meminfo
+splitLine
+
+echo "hard disk information(need root):"
+hdparm -I /dev/sda
+
+splitLine
+
+echo "File system:"
+cat /proc/mounts
+splitLine
